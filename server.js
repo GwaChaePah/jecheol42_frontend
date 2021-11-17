6f935b7defbad4e6031cb5d8b879ae07aa1ec991
Elli-v <osb311@gmail.com>
2021-11-17 19:07:22 +0900

    sever connect

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+const app = express()
+
+//we are configuring dist to serve site files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+ res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`site is listening on port: ${port}`)
